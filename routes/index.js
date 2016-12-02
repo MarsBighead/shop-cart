847eced975ed90916225957fc6d3071a06c10cb3
MarsBighead <duanmhy@126.com>
2016-12-03 00:39:24 +0800

    Update routes and template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var shop = require('../lib/shop');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    shop.showProducts(req, res, next)
+});
+
+router.get('/shop', function(req, res, next) {
+    shop.showProducts(req, res, next)
 });
 
 module.exports = router;
